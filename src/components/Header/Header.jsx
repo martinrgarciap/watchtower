6432a8ee8ca4091d49f5b65b2c218e1530518edb
martinrgarciap <martinrgarciap@gmail.com>
2021-12-04 11:58:07 -0500

    Created header component structure

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,10 +5,14 @@ import logo from "../../assets/logo/WatchTower-logos_black.png";
 export default class Header extends Component {
     render() {
         return (
-            <div className="header">
-                <img className="header__logo" src={logo} alt="Logo" />
-                <h1 className="header__text">Hello</h1>
-            </div>
-        )
+          <div className="header">
+            <img className="header__logo" src={logo} alt="Logo" />
+            <ul className="header__list">
+              <li className="header__list-item">HOME</li>
+              <li className="header__list-item">FORUM</li>
+              <li className="header__list-item">EMERGENCY</li>
+            </ul>
+          </div>
+        );
     }
 }
