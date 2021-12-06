0c8b095b502afc4ec2b8eff284441692da6c8082
martinrgarciap <martinrgarciap@gmail.com>
2021-12-05 20:33:20 -0500

    Created forum page structure and jwt authentication function

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import "./App.scss"
 import LogIn from './pages/LogIn/LogIn';
 import Home from './pages/Home/Home';
-// import Emergency from "./pages/Emergency/Emergency";
-// import Forum from "./pages/Forum/Forum";
+import Emergency from "./pages/Emergency/Emergency";
+import Forum from "./pages/Forum/Forum";
 import SignUp from './pages/SignUp/SignUp';
 import Header from "./components/Header/Header";
 
@@ -14,9 +14,8 @@ function App() {
       <BrowserRouter>
         <Header />
         <Switch>
-          {/* 
+          <Route path="/forum" exact component={Forum} />
           <Route path="/emergency" exact component={Emergency} />
-          <Route path="/forum" exact component={Forum} /> */}
           <Route path="/signup" component={SignUp} />
           <Route path="/login" component={LogIn} />
           <Route path="/" exact component={Home} />
---
+++ src/pages/Emergency/Emergency.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Emergency extends Component {
+    render() {
+        return (
+            <div>
+                Emergency
+            </div>
+        )
+    }
+}
