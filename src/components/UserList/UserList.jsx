0328e2ac69d604156df52652f63140b127108d81
martinrgarciap <martinrgarciap@gmail.com>
2021-12-07 12:02:21 -0500

    Revised home page user card animations with better transitions

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -8,23 +8,23 @@ export default function UserList(props) {
                 props.userList.map((user) => {
                     return (
                       <div className="user-card" key={user._id}>
-                            <p className="user-card__content--username">
-                                {user.username}
-                            </p>
-                            <div className="user-card__hidden">
-                                <p className="user-card__content">
-                                First Name: {user.firstName}
-                                </p>
-                                <p className="user-card__content">
-                                Last Name: {user.lastName}
-                                </p>
-                                <p className="user-card__content">
-                                Phone Number: {user.phone}
-                                </p>
-                                <p className="user-card__content">
-                                Email: {user.email}
-                                </p>
-                            </div>
+                        <p className="user-card__content--username">
+                          {user.username}
+                        </p> 
+                        <div className="user-card__hidden">
+                          <p className="user-card__content">First Name</p>
+                          <p className="user-card__content">{user.firstName}</p>
+                          <p className="user-card__content">Last Name</p>
+                          <p className="user-card__content">{user.lastName}</p>
+                          <p className="user-card__content">Phone Number</p>
+                          <p className="user-card__content">{user.phone}</p>
+                          <p className="user-card__content">
+                            Email
+                          </p>
+                          <p className="user-card__content">
+                            {user.email}
+                          </p>
+                        </div>
                       </div>
                     );
                 })
