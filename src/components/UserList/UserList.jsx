594a6b252436ebfc9d896d4aa2ea460da892f62a
martinrgarciap <martinrgarciap@gmail.com>
2021-12-07 11:35:03 -0500

    Created user list component with user cards and animations

--- src/components/UserList/UserList.jsx
+++ src/components/UserList/UserList.jsx
@@ -8,21 +8,23 @@ export default function UserList(props) {
                 props.userList.map((user) => {
                     return (
                       <div className="user-card" key={user._id}>
-                        <p className="user-card__content">
-                          Username: {user.username}
-                        </p>
-                        <p className="user-card__content">
-                          First Name: {user.firstName}
-                        </p>
-                        <p className="user-card__content">
-                          Last Name: {user.lastName}
-                        </p>
-                        <p className="user-card__content">
-                          Phone Number: {user.phone}
-                        </p>
-                        <p className="user-card__content">
-                          Email: {user.email}
-                        </p>
+                            <p className="user-card__content--username">
+                                {user.username}
+                            </p>
+                            <div className="user-card__hidden">
+                                <p className="user-card__content">
+                                First Name: {user.firstName}
+                                </p>
+                                <p className="user-card__content">
+                                Last Name: {user.lastName}
+                                </p>
+                                <p className="user-card__content">
+                                Phone Number: {user.phone}
+                                </p>
+                                <p className="user-card__content">
+                                Email: {user.email}
+                                </p>
+                            </div>
                       </div>
                     );
                 })
